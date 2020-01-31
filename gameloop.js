033a7a33a6c7bb1a16f2655c9a7109e9297cf78c
Troy Walther <tdgames29@gmail.com>
2020-01-30 23:55:08 -0500

    Changing Colours

--- gameloop.js
+++ gameloop.js
@@ -1,6 +1,9 @@
 
 function gameloop(circle, shader, gl){
-    temp = circle.getRandomPoint();
-    console.log("X: " + temp[0] + " Y: " + temp[1]);
-    return (new Circle(temp[0], temp[1], 0.9, shader, gl));
+    if (Math.floor(Math.random() * 100) == 5){
+        //var temp = [];
+        var temp = circle.getRandomPoint();
+        return (new Circle(parseFloat(temp[0]), parseFloat(temp[1]), 0.15, shader, gl));
+    }
+    return null;
 }
